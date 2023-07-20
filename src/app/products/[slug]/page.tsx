f24ce8513159d613b65b09fad7ae9a2112ebe44c
kimwooyoung <whoyoung90@naver.com>
2023-07-20 23:56:10 +0900

    study: 230720 동적 라우팅 [slug]

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,20 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PantsPage({ params }: Props) {
+  // 동적 라우팅 (props에서 어떤 경로로 들어왔는지 알려준다!)
+  return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+// 동적 라우팅 페이지에서 특정 경로에 한해 미리 페이지를 만들어 두고 싶다면
+export function generateStaticParams() {
+  const products = ["pants", "skirt"]; // 미리 만들어두고 싶은 경로
+  return products.map((product) => {
+    return {
+      slug: product,
+    };
+  });
+}
