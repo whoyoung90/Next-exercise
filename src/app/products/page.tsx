663ee41acd104db308c0dbd380232e7a8e623595
kimwooyoung <whoyoung90@naver.com>
2023-08-01 00:10:21 +0900

    study: 230731 fetch를 사용한 SSG, ISR, SSR, CSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,19 @@
-import { getProducts } from "@/API/products";
 import Link from "next/link";
+import MeowArticle from "@/components/MeowArticle";
+import { getProducts } from "@/API/products";
 
 /* ISR: 몇초 간격으로 SSG를 서버상에서 다시 만들어 둘건지 결정 */
 export const revalidate = 3;
 
 export default async function ProductsPage() {
-  const products = await getProducts(); // 프로미스를 반환하므로 await
+  const products = await getProducts();
+
+  /* ISR */
+  // const res = await fetch("https://meowfacts.herokuapp.com", {
+  //   next: { revalidate: 3 },
+  // });
+  // const data = await res.json();
+  // const factText = data.data[0];
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -16,6 +24,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </>
   );
 }
