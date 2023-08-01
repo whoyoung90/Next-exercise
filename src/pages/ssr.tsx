98015b71012bc2f1b367ec9651ef4a7e0971f18b
kimwooyoung <whoyoung90@naver.com>
2023-08-02 00:12:51 +0900

    study: 230801 12~13버전 비교

---
+++ src/pages/ssr.tsx
@@ -0,0 +1,36 @@
+/* v12 */
+import MeowArticle from "@/components/MeowArticle";
+import { getProducts, Product } from "@/API/products";
+import Link from "next/link";
+
+type Props = {
+  products: Product[];
+};
+
+// 클라이언트에서 실행
+// 콘솔 로그 => 브라우저에 O
+// 이벤트 처리, useEffect 모두 클라이언트에서 동작
+export default function SSGPage({ products }: Props) {
+  return (
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map(({ id, name }, index) => (
+          <li key={index}>
+            <Link href={`/products/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <MeowArticle />
+    </>
+  );
+}
+
+// 서버에서 실행 (SSR)
+// 콘솔 로그 => 브라우저에 X
+export async function getServerSideProps() {
+  const products = await getProducts();
+  return {
+    props: { products },
+  };
+}
